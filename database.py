10b53d2385fb2e087418be197ce61a82bdf25ff2
abdul vahid vahid <71324939+abdulvahidk849@users.noreply.github.com>
2023-03-13 06:12:02 +0000

    job

--- database.py
+++ database.py
@@ -19,3 +19,18 @@ def load_jobs_from_db():
     for row in result.all():
       result_dicts.append(dict(zip(column_names, row)))
     return result_dicts
+
+def load_job_from_db(id):
+  value = []
+  value.append(id)
+  with engine.connect() as conn:
+    query = "SELECT * FROM jobs WHERE id = %s"
+    result = conn.execute(query,value)
+    column_names = result.keys()
+    result_dicts = []
+    
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return result_dicts.append(dict(zip(column_names,rows[0])))
